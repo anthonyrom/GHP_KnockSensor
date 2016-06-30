ea0c77d14383cc2eb448db8c9490f3217ab577f0
Anthony Romaniello <aromaniello70@gmail.com>
2016-06-30 12:33:52 -0400

    Fixed comments
    Removed most of them and some old code.

--- GHP_KnockSensor.ino
+++ GHP_KnockSensor.ino
@@ -18,12 +18,9 @@ void setup() {
 }
 
 void loop() {
-  // read the pushbutton input pin:
   knockState = digitalRead(knockPin);
 
-  // compare the buttonState to its previous state
   if (knockState != lastKnockState) {
-    // if state has changed, increment the counter
     if (knockState == HIGH) {
       // if knock detected
       knockCounter++;
@@ -36,30 +33,6 @@ void loop() {
     }
   }
   lastKnockState = knockState;
-/*
-  if (knockCounter % 3 == 0) {
-    digitalWrite(ledPin0, HIGH);
-  } else {
-    digitalWrite(ledPin0, LOW);
-  }
-
-  if (knockCounter % 5 == 0) {
-    digitalWrite(ledPin1, HIGH);
-  } else {
-    digitalWrite(ledPin1, LOW);
-  }
-
-  if (knockCounter % 2 == 0) {
-    digitalWrite(ledPin2, HIGH);
-  } else {
-    digitalWrite(ledPin2, LOW);
-  }
-
-  if (knockCounter % 7 == 0) {
-    digitalWrite(ledPin3, HIGH);
-  } else {
-    digitalWrite(ledPin3, LOW);
-  }*/
 
   light(ledPin0, 3);
   light(ledPin1, 5);
@@ -76,26 +49,3 @@ void light(int ledPin, int mod) {
   }
   
 }
-
-/*
-Wire up your Knock sensor. Connect it to VCC, GND, and a digital pin.
-Hook up a couple of LEDs to digital pins.
-
-In the setup:
-Set up your LEDs as OUTPUT and the knock sensor as an INPUT
-Define some variables: knockCounter, knockState, lastKnockState
-
-In the loop:
-Digital read the Knock sensor. Send this value to knockState.
-Use some conditionals. If the state has changed then increment the counter,
-Output some text to the serial monitor so you can track when shit happens.
-After this, set lastKnockState equal to the current knockState
-Then use some conditionals to create the FizzBuzz stuff. The modulus operator is useful.
-In these conditionals, turn on the appropriate LEDs.
-
-Upload. Tap breadboard lightly. Watch the serial monitor. Watch the LEDs.
-
-Success.
-
-*/
-
